a176bfaad2321e292698aa5ad59cd99074420f74
Souvik Sarkar <12345souvik12345@gmail.com>
2024-03-18 22:34:40 +0530

    Loader Animation Added

--- preloader/src/components/Loader/index.js
+++ preloader/src/components/Loader/index.js
@@ -3,39 +3,37 @@ import { words } from "./data";
 
 import styles from "./Loader.module.scss";
 import { gsap } from "gsap";
-import { useGSAP } from "@gsap/react";
+import {
+  collapseWords,
+  introAnimation,
+  progressAnimation,
+} from "@/components/Loader/animation";
 
-const Loader = () => {
+const Loader = ({ timeline }) => {
   const loaderRef = useRef(null);
   const progressRef = useRef(null);
   const progressNumberRef = useRef(null);
-  const workGroupsRef = useRef(null);
+  const wordGroupsRef = useRef(null);
 
-  useGSAP(() => {
-    gsap.to(workGroupsRef.current, {
-      yPercent: -80,
-      duration: 5,
-      ease: "power4.inOut",
-    });
-
-    gsap.to(progressRef.current, {
-      scaleX: 1,
-      duration: 5,
-      ease: "power3.inOut",
-    });
-  }, []);
+  useEffect(() => {
+    timeline &&
+      timeline
+        .add(introAnimation(wordGroupsRef))
+        .add(progressAnimation(progressRef, progressNumberRef), 0)
+        .add(collapseWords(loaderRef), "-=1");
+  }, [timeline]);
   return (
-    <div className={styles.loader__wrapper} ref={loaderRef}>
+    <div className={styles.loader__wrapper}>
       <div className={styles.loader__progressWrapper}>
         <div className={styles.loader__progress} ref={progressRef}></div>
         <span className={styles.loader__progressNumber} ref={progressNumberRef}>
           0
         </span>
       </div>
-      <div className={styles.loader}>
+      <div className={styles.loader} ref={loaderRef}>
         <div className={styles.loader__words}>
           <div className={styles.loader__overlay}></div>
-          <div className={styles.loader__wordsGroup} ref={workGroupsRef}>
+          <div className={styles.loader__wordsGroup} ref={wordGroupsRef}>
             {words.map((word, index) => {
               return (
                 <span key={index} className={styles.loader__word}>
